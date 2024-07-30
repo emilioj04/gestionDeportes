d9ef341aba0e04c21e1e777c5d997bf7d0e7362d
Emilio Jaramillo <166522792+emilioj04@users.noreply.github.com>
2024-07-30 17:00:22 -0500

    update

--- ImplementacionDeporte/gestion_campeonato/campeonato/urls.py
+++ ImplementacionDeporte/gestion_campeonato/campeonato/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('crear_campeonato/', views.crear_campeonato, name='crear_campeonato'),
     path('crear_equipo/', views.crear_equipo, name='crear_equipo'),
     path('listar_equipos_por_campeonato/', views.listar_equipos_por_campeonato, name='listar_equipos_por_campeonato'),
+    path('listar_jugadores_equipo/<int:inscripcion_id>/', views.listar_jugadores_equipo, name='listar_jugadores_equipo'),
+    path('agregar_jugador/<int:inscripcion_id>/', views.agregar_jugador, name='agregar_jugador'),
+    path('editar_jugador/<int:jugador_id>/', views.editar_jugador, name='editar_jugador'),
+    path('eliminar_jugador/<int:jugador_id>/', views.eliminar_jugador, name='eliminar_jugador'),
 ]
--- ImplementacionDeporte/gestion_campeonato/campeonato/views.py
+++ ImplementacionDeporte/gestion_campeonato/campeonato/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .forms import *
+
 
 def agregar_deportista(request):
     if request.method == 'POST':
@@ -64,3 +65,42 @@ def listar_equipos_por_campeonato(request):
         equipos = Inscripcion.objects.filter(campeonato=campeonato).select_related('equipo')
         campeonatos_equipos.append((campeonato, equipos))
     return render(request, 'campeonato/listar_equipos_por_campeonato.html', {'campeonatos_equipos': campeonatos_equipos})
+
+def listar_jugadores_equipo(request, inscripcion_id):
+    inscripcion = get_object_or_404(Inscripcion, id=inscripcion_id)
+    jugadores = inscripcion.jugadores.all()
+    return render(request, 'campeonato/listar_jugadores_equipo.html', {'inscripcion': inscripcion, 'jugadores': jugadores})
+
+
+def agregar_jugador(request, inscripcion_id):
+    inscripcion = get_object_or_404(Inscripcion, id=inscripcion_id)
+    if request.method == 'POST':
+        form = InscripcionDeportistaForm(request.POST)
+        if form.is_valid():
+            jugador = form.save()
+            inscripcion.jugadores.add(jugador)
+            return redirect('listar_jugadores_equipo', inscripcion_id=inscripcion_id)
+    else:
+        form = InscripcionDeportistaForm()
+    return render(request, 'campeonato/agregar_jugador.html', {'form': form, 'inscripcion': inscripcion})
+
+
+def editar_jugador(request, jugador_id):
+    jugador = get_object_or_404(Deportista, id=jugador_id)
+    if request.method == 'POST':
+        form = InscripcionDeportistaForm(request.POST, instance=jugador)
+        if form.is_valid():
+            form.save()
+            inscripcion_id = jugador.inscripcion_set.first().id  # Obtener el ID de la inscripción
+            return redirect('listar_jugadores_equipo', inscripcion_id=inscripcion_id)
+    else:
+        form = InscripcionDeportistaForm(instance=jugador)
+    return render(request, 'campeonato/editar_jugador.html', {'form': form, 'jugador': jugador})
+
+def eliminar_jugador(request, jugador_id):
+    jugador = get_object_or_404(Deportista, id=jugador_id)
+    inscripcion_id = jugador.inscripcion_set.first().id  # Obtener el ID de la inscripción
+    if request.method == 'POST':
+        jugador.delete()
+        return redirect('listar_jugadores_equipo', inscripcion_id=inscripcion_id)
+    return render(request, 'campeonato/eliminar_jugador.html', {'jugador': jugador})
